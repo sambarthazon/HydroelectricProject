2fc5331da9ee0e19d27569b2a7125c2e0bb4616d
sabarthazo <sabarthazo@gmail.com>
2022-03-10 10:35:38 -0500

    Update Optimization.py

--- Optimization.py
+++ Optimization.py
@@ -8,34 +8,46 @@ prob = LpProblem("Hydroelectric_Problem", LpMaximize)
 
 # Just for the compilation
 minFlowct = 0
-maxFlowct = 300
+maxFlowct = 100
 
 minWeirct = 0
 maxWeirct = 1000
 
-Pcp = Pct = Xcn = Vc1t = ANCc1t = Xc1t = Yc1t = Vct1 = InitialVolumec = Vct30 = FinalVolumec = 1
+ANCc1t = 300
+# Tableau pour chaque centrale
+InitialVolumec = 360
 
+# faire conversion entre m^2/s en hectom^2/jour
 
 # Creation of the variables
+#pulp.dicts
 
 # Turbined volume at plant 'c' and period 't' between minimum flow and maximum flow
+# Matrice ou Xc1 et Xc2 (déclaré comme des tableaux)
 Xct = LpVariable("TurbinedVolume", minFlowct, maxFlowct, cat="Float")
 
 # Discharged volume at plant 'c' and period 't' between minimum weir and maximum weir
+# Matrice ou Yc1 et Yc2 (déclaré comme des tableaux)
 Yct = LpVariable("DischargedVolume", minWeirct, maxWeirct, cat="Float")
 
 # Volume of the tank at plant 'c' and period 't' 
+# Chaque valeur du tableau vct = lpvariable
 Vct = LpVariable("TankVolume", cat="Float")
 
 # Number of active turbines
 NBctn = LpVariable("ActiveTurbines", cat="Binary")
 
 # Objection Function
-prob += lpSum(lpSum((Pcp)for c in range(1, 2))for p in range(1, 30)), "Objective Function"
+# prob += lpSum(lpSum((2)for c in range(1, 2))for p in range(1, 30)), "Objective Function"
+prob += 2*Xct, "Objective Function"
 
 # Constrains
-prob += Pct <= 2*Xcn, "Power produced for each plant"
-prob += Vc1t == ANCc1t + Vc1t - Xc1t - Yc1t, "Tank volume of the first plant"
-prob += lpSum(NBctn for n in range(1, 3)) == 1, "Single active turbine combination"
-prob += Vct1 == InitialVolumec, "Initial volume in each tank"
-prob += Vct30 == FinalVolumec, "Final volume in each tank"
+# prob += Pct <= 2*Xcn, "Power produced for each plant"
+# Variables en hectom^2/jour (conversion : m2/s = 0,086400 hecto^m2/jour)
+prob += Vct == ANCc1t + Vct - Xct - Yct, "Tank volume of the first plant"
+prob += Vct == InitialVolumec, "Initial volume in each tank"
+# prob += Vct30 == FinalVolumec, "Final volume in each tank"
+
+prob.solve()
+print(pulp.value(Yct))
+print(pulp.value(Xct))

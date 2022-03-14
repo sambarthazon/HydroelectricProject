31bd975f4843415e58f119e826403d4e4f1306a9
sabarthazo <sabarthazo@gmail.com>
2022-03-14 15:51:47 -0400

    Update Optimization.py

--- Optimization.py
+++ Optimization.py
@@ -1,10 +1,24 @@
 from pulp import *
+import pandas as pd
 # Maybe use numpy and panda for data of parameters and sets
 
 print("-------------------------------------")
 
 # Creation of the problem
 prob = LpProblem("Hydroelectric_Problem", LpMaximize)
+
+
+# Pandas
+columns = {'Plant 1': [],
+           'Plant 2': [],
+           'Plant 3': []}
+
+lines = ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7', 'Day 8', 'Day 9', 'Day 10',
+         'Day 11', 'Day 12', 'Day 13', 'Day 14', 'Day 15', 'Day 16', 'Day 17', 'Day 18', 'Day 19', 'Day 20',
+         'Day 21', 'Day 22', 'Day 23', 'Day 24', 'Day 25', 'Day 26', 'Day 27', 'Day 28', 'Day 29', 'Day 30']
+
+pop = pd.DataFrame(columns, index=lines)
+print(pop.index)
 
 # Just for the compilation
 minFlowct = 0

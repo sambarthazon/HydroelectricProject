03ecb2366fd2aa797ad213678afa95874b8ecddc
sabarthazo <sabarthazo@gmail.com>
2022-03-25 16:27:49 -0400

    Solve

--- Optimization.py
+++ Optimization.py
@@ -2,7 +2,6 @@ from pulp import *
 import pandas as pd
 
 
-
 print("\n\n\n------------------ New run ------------------") # Debug
 
 # Creation of the problem
@@ -15,78 +14,87 @@ days = ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7', 'Day 8', 
          'Day 21', 'Day 22', 'Day 23', 'Day 24', 'Day 25', 'Day 26', 'Day 27', 'Day 28', 'Day 29', 'Day 30']
 
 
-# Plant
+# Plants
 plants = ['Plant 1', 'Plant 2']
 
 
-# Tank
-tanks = ['Tank 1', 'Tank 2']
+# Tanks
+tanks = ['Tank']
 
 
 
 # Xct variable : Turbined volume in plant c at day t (minFlowct < columnsXct < maxFlowct)
-# Just for the compilation
-minFlowct = 0
-maxFlowct = 100
 
-columnsXct = {plants[0]: [LpVariable("TurbinedVolume", minFlowct, maxFlowct, cat="Float")],
-           plants[1]: [LpVariable("TurbinedVolume", minFlowct, maxFlowct, cat="Float")]}
+Xct = LpVariable.dicts(name = "Xct",
+                       indices = (range(len(plants)), range(len(days))),
+                       lowBound = 0,
+                       upBound = 100,
+                       cat = "Continuous")
 
-linesXct = days
-
-Xct = pd.DataFrame(columnsXct, index=linesXct)
 # print("\n---------- Xct ----------\n", Xct)
 # End Xct variable
 
 
 
 # Yct variable : Discharged volume in plant c at day t (minWeirct < columnsYct < maxWeirct)
-# Just for the compilation
-minWeirct = 0
-maxWeirct = 1000
 
-columnsYct = {plants[0]: [LpVariable("DischargedVolume", minWeirct, maxWeirct, cat="Float")],
-           plants[1]: [LpVariable("DischargedVolume", minWeirct, maxWeirct, cat="Float")]}
+Yct = LpVariable.dicts(name = "Yct",
+                       indices = (range(len(plants)), range(len(days))),
+                       lowBound = 0,
+                       upBound = 1000,
+                       cat = "Continuous")
 
-linesYct = days
-
-Yct = pd.DataFrame(columnsYct, index=linesYct)
 # print("\n---------- Yct ----------\n", Yct)
 # End Yct variable
 
 
 
 # Vct variable : Tank volume in plant c at day t
-columnsVct = {plants[0]: [LpVariable("100", 100, cat="Float")],
-           plants[1]: [LpVariable("230", 100, cat="Float")]}
 
-linesVct = days
+Vct = LpVariable.dicts(name = "Vct",
+                       indices = (range(len(plants)), range(len(days))),
+                       lowBound = 0,
+                       cat = "Continuous")
 
-Vct = pd.DataFrame(columnsVct, index=linesVct)
-# print("\n---------- Vct ----------\n",, Vct)
+# print("\n---------- Vct ----------\n", Vct)
 # End Vct variable
 
 
 
 # ANCct variable : ??? in plant c at period t
-columnsANCct = {plants[0]: 300,
-           plants[1]: 300}
 
-linesANCct = days
+ANCct = []
+for i in range(len(plants)):
+    array = []
+    for j in range(len(days)):
+        array.append(50)
+    ANCct.append(array)
 
-ANCct = pd.DataFrame(columnsANCct, index=linesANCct)
 # print("\n---------- ANCct ----------\n", ANCct)
 # End ANCct variable
 
 
 
 # InitVolumec variable : initial volume of each plant in each tank
-columnsInitVolume = {plants[0]: 100,
-                     plants[1]: 100}
 
-linesInitVolume = tanks
+InitVolume = [100, 100]
 
-InitVolume = pd.DataFrame(columnsInitVolume, index=linesInitVolume)
+# print("\n---------- InitVolume ----------\n", InitVolume)
+# End InitVolume variable
+
+
+# InitVolumec variable : final volume of each plant in each tank
+
+FinalVolume = [80, 80]
+
+# print("\n---------- FinalVolume ----------\n", FinalVolume)
+# End FinalVolume variable
+
+
+# Turbine vombination variable
+
+numberTurbine = [1, 0, 1]
+
 # print("\n---------- InitVolume ----------\n", InitVolume)
 # End InitVolume variable
 
@@ -95,58 +103,58 @@ InitVolume = pd.DataFrame(columnsInitVolume, index=linesInitVolume)
 # Number of active turbines
 NBctn = LpVariable("ActiveTurbines", cat="Binary")
 
-prob += 2*Xct['Plant 1']['Day 1'], "Objective Function"
+prob += lpSum(lpSum((2*Vct[c][p])for c in range(len(plants)))for p in range(len(days))), "Objective Function"
+
+# Constraints to start with init volume
+prob += Vct[0][0] == InitVolume[0]
+prob += Vct[1][0] == InitVolume[1]
 
 
-itDayPlant1 = iter(days)
-next(itDayPlant1)
-itDayPlant2 = iter(days)
-next(itDayPlant2)
+# Constraints to finish with final volume
+prob += Vct[0][29] == FinalVolume[0]
+prob += Vct[1][29] == FinalVolume[1]
 
-for plant in plants:
-    for day in days:
-        if plant == 'Plant 1':
-            print("Plant", plant)
-            if day == 'Day 1':
-                print("First day", day)
-                print("Constraint for day 1")
-                # V initiale - q + apports
-                print(next(itDayPlant1))
+
+for plant in range(len(plants)):
+    # For each plant
+    if lpSum((NBctn)for n in range(len(numberTurbine))) != 1:
+        break
+    for day in range(len(days)):
+        # For each day
+        if day == 0:
+            # If day 1
+            prob += Vct[plant][day+1] == ANCct[plant][day] + Vct[plant][day] - Xct[plant][day] - Yct[plant][day]
+        elif day < 29:
+            # If day 2 to 29
+            if plant == 0:
+                # If plant 1
+                prob += Vct[plant][day+1] == ANCct[plant][day] + Vct[plant][day] - Xct[plant][day] - Yct[plant][day]
+            elif plant == 1:
+                # If plant 2
+                prob += Vct[plant][day+1] == ANCct[plant][day] + Vct[plant][day] - Xct[plant][day] - Yct[plant][day] + Xct[0][day] + Yct[0][day]
             else:
-                print("Another day", day)
-                if day != 'Day 30':
-                    prob += Vct[plant][next(itDayPlant1)] == ANCct[plant][day] + Vct[plant][day] - Xct[plant][day] - Yct[plant][day]
-                else:
-                    print("Constraint for day 30")
-        elif plant == 'Plant 2':
-            print("Plant", plant)
-            if day == 'Day 1':
-                print("First day", day)
-                print("Constraint for day 1")
-                # V initiale - q + apports
-                print(next(itDayPlant2))
+                # Else
+                print("Error plant")
+        elif day == 29:
+            # If day 30
+            if plant == 0:
+                # If plant 1
+                prob += Vct[plant][29] == FinalVolume[0]
+                prob += Vct[plant][day] == ANCct[plant][day] + Vct[plant][day] - Xct[plant][day] - Yct[plant][day]
+            elif plant == 1:
+                # If plant 2
+                prob += Vct[plant][29] == FinalVolume[1]
+                prob += Vct[plant][day] == ANCct[plant][day] + Vct[plant][day] - Xct[plant][day] - Yct[plant][day] + Xct[0][day] + Yct[0][day]
             else:
-                print("Another day", day)
-                if day != 'Day 30':
-                    prob += Vct[plant][next(itDayPlant2)] == ANCct[plant][day] + Vct[plant][day] - Xct[plant][day] - Yct[plant][day] + Xct['Plant 1'][day] + Yct['Plant 1'][day]
-                else:
-                    print("Constraint for day 30")
+                # Else
+                print("Error plant")
         else:
-            print("Not a plant")
+            # Else
+            print("Error day")
 
 
+prob.solve()
 
 
 # faire conversion entre m^2/s en hectom^2/jour
 # Variables en hectom^2/jour (conversion : m2/s = 0,086400 hecto^m2/jour)
-
-
-
-# Objection Function
-# prob += lpSum(lpSum((2)for c in range(1, 2))for p in range(1, 30)), "Objective Function"
-
-
-
-
-# print("\n---------- Xct ----------\n", Xct)
-# print("\n---------- Yct ----------\n", Yct)
